786213958236196e4627575e4af160c21c1d9a20
Alejandro Esquivel <yoquiale@gmail.com>
2025-04-05 22:28:26 +0200

    Commit inicial con componentes de rover y grid

---
+++ src/components/MarsRover.vue
@@ -0,0 +1,13 @@
+<script setup>
+import RoverGrid from './RoverGrid.vue';
+
+</script>
+
+<template>
+  <section>
+    <header class="m-4 rounded-md w-11/12 mx-auto p-4 bg-slate-600">
+      <h1 class="text-3xl text-green-400 font-bold text-center">Mars Rover Mission</h1>
+    </header>
+    <RoverGrid />
+  </section>
+</template>
